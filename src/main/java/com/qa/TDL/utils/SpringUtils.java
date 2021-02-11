2cca82202de73e7d8995e9988757510ad1508147
Arsalan Asad <https://gitlab.com/arsalanasad96/python-pre-learning-tasks-published/-/tree/master>
2021-02-11 09:57:48 +0000

    utils created

--- src/main/java/com/qa/TDL/utils/SpringUtils.java
+++ src/main/java/com/qa/TDL/utils/SpringUtils.java
@@ -1,31 +1,28 @@
-package com.qa.TDL.utils;
-
-import static org.springframework.beans.BeanUtils.copyProperties;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.validation.metadata.PropertyDescriptor;
-
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-
-public class SpringUtils {
-
-	public static void mergeNotNull(Object src, Object trgt) {
-		copyProperties(src, trgt, getNullPropName(src));
-	}
-
-	private static String[] getNullPropName(Object src) {
-		final BeanWrapper wrappedSrcObj = new BeanWrapperImpl(src);
-		Set<String> pName = new HashSet<>();
-		for (PropertyDescriptor desc : wrappedSrcObj.getPropertyDescriptors()) {
-			if (wrappedSrcObj.getPropertyValue(desc.getName()) == null)
-				pName.add(desc.getName());
-
-		}
-		return pName.toArray(new String[pName.size()]);
-		}
-	}
-
-}
+//package com.qa.TDL.utils;
+//
+//import static org.springframework.beans.BeanUtils.copyProperties;
+//
+//import java.beans.PropertyDescriptor;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import org.springframework.beans.BeanWrapper;
+//import org.springframework.beans.BeanWrapperImpl;
+//
+//public class SpringUtils {
+//
+//	public static void mergeNotNull(Object src, Object trgt) {
+//		copyProperties(src, trgt, getNullPropName(src));
+//	}
+//
+//	private static String[] getNullPropName(Object src) {
+//		final BeanWrapper wrappedSrcObj = new BeanWrapperImpl(src);
+//		Set<String> pName = new HashSet<>();
+//		for (PropertyDescriptor desc : wrappedSrcObj.getPropertyDescriptors()) {
+//			if (wrappedSrcObj.getPropertyValue(desc.getName()) == null)
+//				pName.add(desc.getName());
+//
+//		}
+//		return pName.toArray(new String[pName.size()]);
+//	}
+//}
